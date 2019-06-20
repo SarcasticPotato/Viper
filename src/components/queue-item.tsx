37f7ec3124a018c0a15fdbd0028090afb7d2f277
SarcasticPotato <moritz.j.egger@gmail.com>
2019-06-20 16:03:03 +0200

    Add placeholder android app Icon (ionicons download icon)

--- src/components/queue-item.tsx
+++ src/components/queue-item.tsx
@@ -22,7 +22,7 @@ export class QueueItemComponent extends React.Component<Props> {
 
     return (
       <ListItem button onPress={(_) => this.goToDetailPage()}>
-        <Text>{item.name} ({item.linksdone/item.linkstotal*100}%)</Text>
+        <Text>{item.name} ({Number(item.linksdone/item.linkstotal*100).toPrecision(4)}%)</Text>
       </ListItem>
     );
   }
